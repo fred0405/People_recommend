88e8b1f8e534e007c0b1b14482151e15725b0ba3
fred0405 <fredliu0405@gmail.com>
2019-07-18 13:58:11 +0800

    daily update

--- data_clean.py
+++ data_clean.py
@@ -1,4 +1,19 @@
 import re
+import sys
 from os import listdir
 
-files = listdir("./subtitles")
+# files = listdir("./subtitles")
+
+# for file in files:
+# 	print(file.split('.')[0])
+# 	path = "./subtitles/" + file
+# 	with open(path, 'r') as f:
+# 		for line in f:
+# 			print(re.findall(r'[\u4E00-\u9FBF]+', line))
+			
+path = "./subtitles/Table_Games_Taichung.txt"
+with open(path, 'r') as f:
+	print(f)
+	for line in f:
+		#print(line)
+		print(re.findall(r'[\u4E00-\u9FBF]+', line))
