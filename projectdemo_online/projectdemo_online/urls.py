98503f0ad4c553a41e2c91aa1d70fe14510f2ed4
fred0405 <fredliu0405@gmail.com>
2019-10-07 12:55:07 +0800

    webpage

--- projectdemo_online/projectdemo_online/urls.py
+++ projectdemo_online/projectdemo_online/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from web.views import index
+from web.views import index, rshow
 urlpatterns = [
 	path('', index),
 	path('index/', index),
     path('admin/', admin.site.urls),
+    path('rshow/', rshow),
 ]
--- projectdemo_online/web/views.py
+++ projectdemo_online/web/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def index(request):
 	context = {}
 	context['hello'] = 'Hello World!' 
 	return render(request, 'index.ejs', context)
+@csrf_exempt
+def rshow(request):
+	print(request.POST['keyword'])
+	return JsonResponse({"status": True})
