465f85671362d3c3a96fdb921e91c21169e8fc5a
fred0405 <fredliu0405@gmail.com>
2019-11-18 11:05:39 +0800

    add new youtuber, build youtuber index

--- cut_word_and_data_clean.py
+++ cut_word_and_data_clean.py
@@ -15,6 +15,8 @@ done = []
 youtubers = ['subtitles/A_Di_English.txt', 'subtitles/CYFIT.txt', 'subtitles/Dodo_Village.txt', 'subtitles/Empty_Bottle_King.txt',
 'subtitles/Gamker.txt', 'subtitles/Hello_Catie.txt', 'subtitles/Huan.txt', 'subtitles/Little_Hot_Sing.txt',
 'subtitles/Lulu.txt', 'subtitles/Table_Games_Taichung.txt']
+# youtubers = ['subtitles_2/菜喳.txt', 'subtitles_2/蔡阿嘎.txt', 'subtitles_2/小玉.txt', 'subtitles_2/maomaotv.txt', 'subtitles_2/gayi.txt', 'subtitles_2/眾量級CROWD.txt', 'subtitles_2/missga.txt', 'subtitles_2/bq.txt', 'subtitles_2/這群人TGOP.txt', 'subtitles_2/聖結石Saint.txt', 'subtitles_2/人生肥宅x尊.txt']
+
 # with open('text/CYFIT.txt', 'r') as input:
 for youtuber in youtubers:
 	data = dict()
@@ -36,7 +38,7 @@ for youtuber in youtubers:
 			# if(terms != []):
 				# done += terms
 	for key in data:
-		path = 'documents/' + key + '.txt'
+		path = 'documents/' + key + '@@' + youtuber.split('/')[1]
 		with open(path, 'w', encoding='utf8') as out:
 			# doc = key + '\n'
 			doc = ''
--- projectdemo_online/web/views.py
+++ projectdemo_online/web/views.py
@@ -27,13 +27,14 @@ def search(request):
 	print(query)
 	# rtn_dict = get_result(query)
 	rtn_dict = get_result_new(query)
+	print(rtn_dict)
 	return JsonResponse({"status": True, "result": json.dumps(rtn_dict)})
 
 def get_result(query_str):
 	file_index_path =  os.path.abspath("web/static/whoosh/")# the main contents     where you put your file what you change this line
-	file_movie_path = file_index_path + "/documents_movie"
+	file_movie_path = file_index_path + "/documents_movie_all"
 	result_dict = {}
-	schema = Schema(title=TEXT(stored=True), content=TEXT(stored=True), movie_id = TEXT(stored = True), path=ID(stored=True) )
+	schema = Schema(title=TEXT(stored=True), content=TEXT(stored=True), movie_id = TEXT(stored = True), youtuber = TEXT(stored = True), path=ID(stored=True) )
 	index_file = file_index_path + "/indexdir"
 	if not os.path.exists(index_file):
 		os.mkdir(index_file)
@@ -41,20 +42,25 @@ def get_result(query_str):
 	ix = create_in(index_file, schema)
 	writer = ix.writer()
 
+
+# 【Huan】 巴掌大的電腦可以幹嘛 ECS LIVA Z2開箱測試-V-fIGqXdNCc.wav@@Huan.txt
 	filepaths = [os.path.join(file_movie_path,i) for i in os.listdir(file_movie_path)]
 	for path in filepaths:
 	    fp = open(path,'r',encoding="utf-8")
-	    name = fp.name.split(file_movie_path+"/")[1].split(".wav.txt")[0]
+	    name = fp.name.split(file_movie_path+"/")[1].split(".wav@@")[0]
 	    title = name[0:-12]
+	    
+	    youtuber = fp.name.split(file_movie_path+"/")[1].split(".wav@@")[1].split('.txt')[0]
+	    print(youtuber)
 	    try:
 	        ID_ = name[-11:]
 	    except:
 	        ID_ = "missing data"
 	    text = fp.read()
-	    writer.add_document(title=title,content=text , movie_id=ID_ , path = u"/b" )
+	    writer.add_document(title=title,content=text , movie_id=ID_ , youtuber = youtuber, path = u"/b" )
 	writer.commit()
 
-	topN = 10
+	topN = 100
 	with ix.searcher(weighting=scoring.BM25F) as searcher:
 	    query = QueryParser("content", ix.schema).parse(query_str)
 	    #query = MultifieldParser( ["content" ] , ix.schema).parse(query_str)
@@ -62,7 +68,7 @@ def get_result(query_str):
 
 	    # for i in range(topN):
 	    for i in range(topN):
-	    	result_dict[results[i]['movie_id']] = {"title": results[i]['title'], "score": str(results[i].score)}
+	    	result_dict[results[i]['movie_id']] = {"title": results[i]['title'], "youtuber":results[i]['youtuber'], "score": str(results[i].score)}
 	        # print("title : " + results[i]['title'] + "\n" + "ID : " + results[i]['movie_id'] + "\n" + str(results[i].score))
 	print(result_dict)
 	return result_dict
@@ -72,7 +78,7 @@ def get_result_new(query_str):
 	result_dict = {}
 	# print(index.exists_in(file_path))
 	ix = open_dir(file_path)
-	topN = 10
+	topN = 100
 	with ix.searcher(weighting=scoring.BM25F) as searcher:
 	    query = QueryParser("content", ix.schema).parse(query_str)
 	    #query = MultifieldParser( ["content" ] , ix.schema).parse(query_str)
@@ -80,7 +86,7 @@ def get_result_new(query_str):
 
 	    # for i in range(topN):
 	    for i in range(topN):
-	    	result_dict[results[i]['movie_id']] = {"title": results[i]['title'], "score": str(results[i].score)}
+	    	result_dict[results[i]['movie_id']] = {"title": results[i]['title'], "youtuber":results[i]['youtuber'], "score": str(results[i].score)}
 	        # print("title : " + results[i]['title'] + "\n" + "ID : " + results[i]['movie_id'] + "\n" + str(results[i].score))
 	print(result_dict)
 	return result_dict
